340c76a6481573abc962b11a810805b6807f1c68
wdong20 <wdong20@stuy.edu>
2020-12-10 12:15:30 -0500

    Added add(element) override; still untested

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,4 +1,11 @@
 import java.util.ArrayList;
 
 public class NoNullArrayList<T> extends ArrayList<T> {
+    public boolean add(T element) {
+        if (element == null) {
+            throw new IllegalArgumentException("Cannot add a null into a NoNullArrayList.");
+        }
+
+        return add(element);
+    }
 }
