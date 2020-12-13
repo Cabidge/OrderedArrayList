57abedce4c36a9d501c026b28fa370136a45ac77
wdong20 <wdong20@stuy.edu>
2020-12-13 11:09:56 -0500

    Added both constructors (default and initialCapacity)

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,6 +1,14 @@
 import java.util.ArrayList;
 
 public class NoNullArrayList<T> extends ArrayList<T> {
+    public NoNullArrayList() {
+        super();
+    }
+
+    public NoNullArrayList(int initialCapacity) {
+        super(initialCapacity);
+    }
+
     public boolean add(T element) {
         if (element == null) {
             throw new IllegalArgumentException("Cannot add a null into a NoNullArrayList.");
