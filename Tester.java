1af92ec943f22367d39410633158a143175c4229
wdong20 <wdong20@stuy.edu>
2020-12-13 10:56:57 -0500

    Tested both add() methods and exception throwing - working as intended

--- Tester.java
+++ Tester.java
@@ -1,5 +1,44 @@
 public class Tester {
     public static void main(String[] argargargs) {
-        
+        /*
+        For more clarity
+        ✔ - Correct
+        ✘ - Bad
+        */ 
+
+        // NoNullArrayList testing
+        NoNullArrayList<Integer> a = new NoNullArrayList<Integer>();
+
+        // A. Can add normally?
+        try {
+            a.add(Integer.valueOf(5));
+            System.out.println("✔ Added 5");
+        } catch (Exception e) {
+            Tester.printBad('A');
+        }
+        try {
+            a.add(0, Integer.valueOf(69));
+            System.out.println("✔ Added 69 (nice) to the start");
+        } catch (Exception e) {
+            Tester.printBad('A');
+        }
+
+        // B. Throws error?
+        try {
+            a.add(null);
+            Tester.printBad('B');
+        } catch (Exception e) {
+            System.out.println("✔ Threw error correctly");
+        }
+        try {
+            a.add(0, null);
+            Tester.printBad('A');
+        } catch (Exception e) {
+            System.out.println("✔ Threw error correctly");
+        }
+    }
+
+    public static void printBad(char section) {
+        System.out.println("✘ Whoops! Something went wrong in section " + section);
     }
 }
