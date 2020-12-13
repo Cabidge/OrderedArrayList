65a41038347fc317776ebab586bce14bc8e07ee9
wdong20 <wdong20@stuy.edu>
2020-12-13 11:05:30 -0500

    Updated tests to include set() method

--- Tester.java
+++ Tester.java
@@ -10,35 +10,59 @@ public class Tester {
         NoNullArrayList<Integer> a = new NoNullArrayList<Integer>();
 
         // A. Can add normally?
+        Tester.printSection('A');
         try {
             a.add(Integer.valueOf(5));
             System.out.println("✔ Added 5");
-        } catch (Exception e) {
+        } catch (RuntimeException e) {
             Tester.printBad('A');
         }
         try {
             a.add(0, Integer.valueOf(69));
             System.out.println("✔ Added 69 (nice) to the start");
-        } catch (Exception e) {
+        } catch (RuntimeException e) {
+            Tester.printBad('A');
+        }
+        try {
+            a.set(1, Integer.valueOf(99));
+            System.out.println("✔ Set the last value to 99");
+        } catch (RuntimeException e) {
             Tester.printBad('A');
         }
 
         // B. Throws error?
+        Tester.printSection('B');
         try {
             a.add(null);
             Tester.printBad('B');
-        } catch (Exception e) {
-            System.out.println("✔ Threw error correctly");
+        } catch (IllegalArgumentException e) {
+            System.out.println("✔ Task failed successfully");
+        } catch (RuntimeException e) {
+            Tester.printBad('B');
         }
         try {
             a.add(0, null);
-            Tester.printBad('A');
-        } catch (Exception e) {
-            System.out.println("✔ Threw error correctly");
+            Tester.printBad('B');
+        } catch (IllegalArgumentException e) {
+            System.out.println("✔ Task failed successfully");
+        } catch (RuntimeException e) {
+            Tester.printBad('B');
+        }
+        try {
+            a.set(0, null);
+            Tester.printBad('B');
+        } catch (IllegalArgumentException e) {
+            System.out.println("✔ Task failed successfully");
+        } catch (RuntimeException e) {
+            Tester.printBad('B');
         }
     }
 
     public static void printBad(char section) {
         System.out.println("✘ Whoops! Something went wrong in section " + section);
+    }
+
+    public static void printSection(char section) {
+        System.out.println("\nSection " + section + ":");
     }
 }
